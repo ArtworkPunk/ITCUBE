ac249d7e1c90f0d493d047ec1ba6aaa01ec104c2
Punk <67797794+ArtworkPunk@users.noreply.github.com>
2024-02-01 15:37:40 +0300

    Add files via upload

---
+++ itcubeJavaScript.js
@@ -0,0 +1,56 @@
+function downscroll(){
+    window.scrollTo(0,977)
+}
+
+function downscroll2(){
+    window.scrollTo(0,1954)
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var hiddenText = document.getElementById('SECRET');
+    var toggleButton = document.getElementById('infobtn3');
+
+    window.addEventListener('scroll', function () {
+        var scrollPosition = window.scrollY;
+
+        var pixelThreshold = 2900;
+
+        if (scrollPosition >= pixelThreshold) {
+            hiddenText.style.opacity = 1;
+        } else {
+            hiddenText.style.opacity = 0;
+        }
+
+        toggleButton.addEventListener('click', function () {
+            hiddenText.style.opacity = 0;
+    });
+    });
+});
+
+function downscroll3(){
+    window.scrollTo(0,3908)
+    document.getElementById("popup").style.display = "block";
+}
+
+
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+    window.scrollTo(0,1954)
+}
+
+function submitForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+
+    if (name.trim() === "" || email.trim() === "") {
+        alert("Ошибка: Введите корректные данные во все поля");
+        return;
+    }
+
+    alert("Данные успешно отправлены!");
+
+    closePopup();
+    location.reload();
+    window.scrollTo(0,0);
+}
